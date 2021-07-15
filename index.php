31f2fb2a8e8c01b18607298555f32a3b78f16a3a
popo03 <83389638+popo03@users.noreply.github.com>
2021-07-15 16:25:34 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css">
+<title>simple_bbs</title>
+</head>
+
+<body> 
+<div class="wrapper">
+  <div class="side_bar">
+    <p><a href="login.php">ログイン</a></p>
+    <p><a href="create.php">新規ユーザー登録</a></p>
+  </div>
+  <div class="main">
+    <h1>Simple BBS</h1>
+    <pre>
+      test
+    </pre>
+  </div>
+</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css">
+<title>simple_bbs</title>
+</head>
+
+<body> 
+<div class="wrapper">
+ログイン画面
+<a href="index.php">戻る</a>
+</div>
+</body>
+</html>

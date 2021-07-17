1986a6ff1fba50fe3afcf871d9df4d6846c39da2
popo03 <83389638+popo03@users.noreply.github.com>
2021-07-17 17:46:24 +0900

    メッセージの保存

--- input_do.php
+++ input_do.php
@@ -14,10 +14,16 @@
     <h1>Simple BBS</h1>
       <?php
       require('dbconnect.php');
+
+        $statement = $db->prepare('INSERT INTO posts SET message=?, created_at=NOW()');
+        $statement->bindParam(1, $_POST['message']);
+        $statement->execute();
+        echo 'メッセージが登録されました';
+
       ?>
-<article>
-      確認画面
-</article>
+      <pre>
+        <a href="room.php">トップページに戻る</a>
+      </pre>
   </div>
 </div>
 </body>

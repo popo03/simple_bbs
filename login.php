0262e1e47b48086f6a2441f0edee43a3425ce54d
popo03 <83389638+popo03@users.noreply.github.com>
2021-07-19 12:40:06 +0900

    ログイン画面の実装

--- login.php
+++ login.php
@@ -1,3 +1,32 @@
+<?php
+require('dbconnect.php');
+
+if (!empty($_POST)) {
+  if ($_POST['email'] === '') {
+    $error['email'] = 'blank';
+  }
+  if ($_POST['password'] === '') {
+    $error['password'] = 'blank';
+  }
+
+  if ($_POST['email'] !== '' && $_POST['password'] !== '') {
+    $login = $db->prepare('SELECT * FROM users WHERE email=? AND password=?');
+    $login->execute(array(
+      $_POST['email'],
+      sha1($_POST['password'])
+    ));
+    $user = $login->fetch();
+
+    if ($user) {
+      header('Location: room.php');
+      exit();
+    } else {
+      $error['login'] = 'failed';
+    }
+  } 
+}
+
+?>
 <!doctype html>
 <html lang="ja">
 <head>
@@ -12,9 +41,30 @@
 
   </div>
   <div class="main">
-    ログイン画面
-  </div>
+    <p><h2>ログイン画面</h2></p>
+    <form action="" method="post">
+      <dl>
+        <dt>メールアドレス</dt>
+        <dd>
+          <input type="text" name="email" value="<?php print(htmlspecialchars($_POST['email'], ENT_QUOTES)); ?>">
+          <?php if ($error['email'] === 'blank'): ?>
+            <p>*メールアドレスを入力して下さい</p>
+          <?php endif; ?>
+        </dd>
+        <dt>パスワード</dt>
+        <dd>
+          <input type="password" name="password" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>">
+          <?php if ($error['password'] === 'blank'): ?>
+            <p>*パスワードを入力して下さい</p>
+          <?php endif; ?>
+          <?php if ($error['login'] === 'failed'): ?>
+            <p>*ログインに失敗しました。正しく入力して下さい</p>
+          <?php endif; ?>
+        </dd>
+      </dl>
+    <input type="submit" value="ログインする">
     <a href="index.php">戻る</a>
+  </div>
 </div>
 </body>
 </html>
